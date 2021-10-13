9a0cb4398ff822d21b47e538dd760ebc6d5f4b59
Shane McDonough <shanemcdman@gmail.com>
2021-10-13 00:57:04 -0700

    Finish parse_grid function

--- parse_grid.py
+++ parse_grid.py
@@ -3,6 +3,15 @@
 from typing import List
 from PIL import Image
 
+def color_almost_eq(a: (int, int, int), b: (int, int, int)) -> bool:
+    '''
+    returns if two rgb colors are almost the same color
+    no more than 5 difference between two same vals
+    :a: first color value rgb format
+    :b: second color value rgb format
+    '''
+    return abs(a[0] - b[0]) <= 5 and abs(a[1] - b[1]) <= 5 and abs(a[2] - b[2]) <= 5
+
 def parse_grid(filename: str, grid_size: (int, int), offset: (int, int) = (0, 0)) -> str:
     '''
     parses an image and converts it into a grid of 'x' and '.'
@@ -17,16 +26,26 @@ def parse_grid(filename: str, grid_size: (int, int), offset: (int, int) = (0, 0)
             )
     pixels = img.load()
     result = ''
+    color1 = None
+    color2 = None
     for i in range(grid_size[1]):
         for j in range(grid_size[0]):
             pixel = pixels[
                 offset[0] + j * cell_size[0],
                 offset[1] + i * cell_size[1]
                 ]
-            if pixel[0] == 255:
+            if color1 is None:
+                color1 = pixel
                 result += '.'
-            else:
+            elif color2 is None and not color_almost_eq(color1, pixel):
+                color2 = pixel
                 result += 'x'
+            elif color_almost_eq(color1, pixel):
+                result += '.'
+            elif color_almost_eq(color2, pixel):
+                result += 'x'
+            else:
+                raise ValueError('Third color detected')
         result += '\n'
     return result[:-1]
 
