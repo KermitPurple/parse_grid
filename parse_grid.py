76ac8ad142f54c534a2ded10a40180edb5c8c572
Shane McDonough <shanemcdman@gmail.com>
2021-10-12 17:39:04 -0700

    create parse grid function

--- parse_grid.py
+++ parse_grid.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import List
+from PIL import Image
 
 def write_str_list(lines: List[str], filename: str):
     '''
@@ -20,8 +21,36 @@ def write_str(text: str, filename: str):
     with open(filename, 'w') as f:
         f.write(text)
 
+def parse_grid(filename: str, grid_size: (int, int), offset: (int, int) = (0, 0)) -> str:
+    '''
+    parses an image and converts it into a grid of 'x' and '.'
+    :filename: name of file to open
+    :offset:
+    :grid_size:
+    '''
+    img = Image.open(filename)
+    cell_size = (
+            img.size[0] / grid_size[0],
+            img.size[1] / grid_size[1]
+            )
+    pixels = img.load()
+    result = ''
+    for i in range(grid_size[1]):
+        for j in range(grid_size[0]):
+            pixel = pixels[
+                offset[0] + j * cell_size[0],
+                offset[1] + i * cell_size[1]
+                ]
+            if pixel[0] == 255:
+                result += '.'
+            else:
+                result += 'x'
+        result += '\n'
+    return result[:-1]
+
 def main():
     '''driver code'''
+    print(parse_grid('conway.png', (38 ,11), (7, 7)))
 
 if __name__ == "__main__":
     main()
